8dfdc2e84338fd04132671003e52b043bbe331da
satori-lunar <elliotmccormick@satori-lunar.com>
2025-12-22 05:44:32 -0500

    Remove Test Class from code and delete helper SQL

--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -157,8 +157,7 @@ export default function Classes() {
       'The Heart of Nourishment',
       'Foundations in Motion',
       'Hatha Yoga',
-      'Seedlings',
-      'Test Class'
+      'Seedlings'
     ],
     'Tuesday': [
       'Inner Chords',
